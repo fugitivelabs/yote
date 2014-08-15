99dcf96a6091cebf4a9d7af274064c7c392e1e67
grantfowler <grantfowler@gmail.com>
2014-08-15 10:47:45 -0400

    fix initial post creation

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
   ;
 
-//define product schema
+//define post schema
 var postSchema = mongoose.Schema({
   title:            { type: String, required: '{PATH} is required!' }
   , content:        { type: String, required: '{PATH} is required!' }
@@ -11,11 +11,11 @@ var postSchema = mongoose.Schema({
 
 Post = mongoose.model('Post', postSchema);
 
-//product model methods
+//post model methods
 function createDefaults() {
   Post.find({}).exec(function(err, posts) {
     if(posts.length == 0) {
-      Post.create({title: "Fugitive Labs Introduces Coyote!", description: "A neat-o new product that helps you build apps on the MEAN stack!", featured: true});
+      Post.create({title: "Fugitive Labs Introduces Coyote!", content: "A neat-o new product that helps you build apps on the MEAN stack!", featured: true});
       console.log("created initial post defaults");
     }
   });
