a255a983db05744d0d59f089e53d36080cb1a8ce
Erik Guffrey <erik.guffrey@gmail.com>
2014-09-08 12:27:49 -0400

    working

--- public/app/app.js
+++ public/app/app.js
@@ -4,8 +4,11 @@
 console.log('angular application loaded');
 
 angular.module('Yote', [
+  // add in custom dependencies here
   'ngRoute'
+  , 'ngTouch'
   , 'ui.router'
-]);
+])
 
-
+// end of file
+;
