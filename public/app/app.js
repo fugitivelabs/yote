7e89f97aaffa67759259b6bb3a7849f0a32f5360
Erik Guffrey <erik.guffrey@gmail.com>
2014-09-08 12:21:39 -0400

    adding in ui-routes.js

--- public/app/app.js
+++ public/app/app.js
@@ -6,19 +6,6 @@ console.log('angular application loaded');
 angular.module('Yote', [
   'ngRoute'
   , 'ui.router'
-]).config(function($routeProvider, $locationProvider, $stateProvider, $urlRouterProvider){
-  console.log('configure ui router');
-  
-  $locationProvider.html5Mode(true);
-  
-  $urlRouterProvider.otherwise('/');
-
-  $stateProvider
-    .state('root', {
-      abstract: true
-      , url: '/'
-      , templateUrl: '/views/layouts/default'
-    })
-});
+]);
 
 
