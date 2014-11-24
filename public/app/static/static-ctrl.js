60680c83c896422799633a2f5deace7e875a1e62
Chance Griffin <chance.griffin@gmail.com>
2014-11-24 14:21:19 -0500

    removed the Jquery method, added in default mobile styles for navList

--- public/app/static/static-ctrl.js
+++ public/app/static/static-ctrl.js
@@ -7,24 +7,21 @@ angular.module('Yote')
   .controller('NavCtrl', ['$scope', '$stateParams', '$state', 'UserFactory', 'PostFactory', function($scope, $stateParams, $state, UserFactory, PostFactory){
     console.log("NavCtrl loaded...");
 
+    console.log("hi");
     $scope.openMenu = true;
 
-    $scope.toggleMenu = function($scope) {
-      console.log("hello");
-    }
 
   }])
 
   .controller('StaticCtrl', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state){
     console.log("StaticCtrl loaded...");
+
+
   }])
 
   .controller('HomeCtrl', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state){
     console.log("HomeCtrl loaded...");
 
-    // $('.burger').click(function() {
-    //   $(this).toggleClass('active');
-    // });
 
   }])
 
