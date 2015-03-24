a6ba3229b4a33ffb263a2b1eb088a0b7e2a58d87
Grant Fowler <grantfowler@gmail.com>
2015-03-24 11:49:51 -0400

    post to id and change 'show' to be more specific

--- server/router/api/post-api.js
+++ server/router/api/post-api.js
@@ -9,18 +9,19 @@ var posts = require('../../controllers/posts');
 module.exports = function(router, requireLogin, requireRole) {
   
   // - Create
-  router.post('/api/posts'          , requireLogin(), posts.create);
+  router.post('/api/posts'              , requireLogin(), posts.create);
   
   // - Read
-  router.get('/api/posts'           , posts.list);
-  router.get('/api/posts/search'    , posts.search);
-  router.get('/api/posts/byId/:id'  , posts.getById); 
-  router.get('/api/posts/:slug'     , posts.getBySlug);
-  
+  router.get('/api/posts'               , posts.list);
+  router.get('/api/posts/search'        , posts.search);
+  router.get('/api/posts/bySlug/:slug'  , posts.getBySlug);
+  router.get('/api/posts/populate/:id'  , posts.getAndPopulate);
+  router.get('/api/posts/:id'           , posts.getById); 
+
   // - Update
-  router.put('/api/posts/:slug'     , requireLogin(), posts.update); // must login as post owner to update the post
+  router.put('/api/posts/:id'           , requireLogin(), posts.update); // must login as post owner to update the post
   
   // - Delete
-  router.delete('/api/posts/:slug'  , requireRole('admin'), posts.delete); // must be an 'admin' to delete
+  router.delete('/api/posts/:id'        , requireRole('admin'), posts.delete); // must be an 'admin' to delete
 
 }
