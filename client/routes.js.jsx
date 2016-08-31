c541b1220e550e2733eecd049fef2e2dfc0a88cf
ffugly <erik@fugitivelabs.com>
2016-08-31 16:30:16 -0400

    add in 404 not found page because i'm always hitting dead routes

--- client/routes.js.jsx
+++ client/routes.js.jsx
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 //import custom components
 import Root from './global/components/Root.js.jsx';
 import Landing from './modules/static/components/landing/Landing.js.jsx';
+import NotFound from './modules/static/components/NotFound.js.jsx';
 
 const routes =
       <Route path="/" component={Root} >
@@ -11,6 +12,7 @@ const routes =
         { Object.keys(moduleRoutes).map((moduleName, i) =>
           moduleRoutes[moduleName]
         )}
+        <Route path="/*" component={NotFound} />
       </Route>
 ;
 
