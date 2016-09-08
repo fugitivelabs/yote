6453d25c353f3d0e63a671fa88ba50d30ff4f1f4
Grant Fowler <grantfowler@gmail.com>
2016-09-08 17:21:38 -0400

    packages and env

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ var secrets = require('../secrets.js');
 
 //change this to change the name of your mongodb database name
 var dbName = "yote";
-// var remoteDb = "10.240.0.29"; //TODO: grab from ENV variable
+//option to set database location manually via environment variables
 var remoteDb = process.env.REMOTE_DB ? process.env.REMOTE_DB : false;
 
 module.exports = {
@@ -29,14 +29,4 @@ module.exports = {
     , port: process.env.PORT || 80
     , secrets: secrets || {}
   }
-  // , production: {
-  //   db: process.env.MONGODB_PORT ? process.env.MONGODB_PORT.replace("tcp", "mongodb") + "/" + dbName : 'mongodb://' + dbLocation + '/' + dbName
-  //   , redis: {
-  //     host: process.env.REDIS_PORT ? process.env.REDIS_PORT.replace("tcp://", "").split(":")[0] : "localhost"
-  //     , port: process.env.REDIS_PORT ? process.env.REDIS_PORT.replace("tcp://", "").split(":")[1] : "6379"
-  //   }
-  //   , rootPath: rootPath
-  //   , port: process.env.PORT || 80
-  //   , secrets: secrets || {}
-  // }
 }
