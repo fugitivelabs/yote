16546b4b0a9d7c0b27959be08d45ff841b722ca7
Grant Fowler <grantfowler@gmail.com>
2016-10-25 22:41:43 -0400

    mongoose and depreciated promise library

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
   ;
 
 module.exports = function(config) {
+  mongoose.Promise = global.Promise; //mongoose internal Promise library depreciated
   mongoose.connect(config.db);
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'mongo connection error'));
