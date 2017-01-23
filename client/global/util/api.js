e3f69762096009ab6169c58c542ad20d51f5058a
Grant Fowler <grant@fugitivelabs.com>
2017-01-23 16:19:28 -0500

    allow custom headers in callapi

--- client/global/util/api.js
+++ client/global/util/api.js
@@ -2,13 +2,12 @@ import fetch from 'isomorphic-fetch';
 
 const baseUrl = ""; //later required for server rendering
 
-export default function callAPI(route, method = 'GET', body) {
+export default function callAPI(route, method = 'GET', body, headers = { 
+    'Accept': 'application/json', 'Content-Type': 'application/json'
+  }) {
   console.log("CALLING API FUNCTION");
   return fetch(baseUrl + route, {
-    headers: { 
-      'Accept': 'application/json'
-      , 'Content-Type': 'application/json'
-    }
+    headers
     , method
     , credentials: 'same-origin'
     , body: JSON.stringify(body)
