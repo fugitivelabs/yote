a9cdf56b24685f339158b1e08dd4c65c90abc746
Erik Guffrey <erik@fugitivelabs.com>
2017-03-21 17:38:47 -0400

    rename adminSidenav to AdminSideNav

---
+++ client/modules/admin/components/AdminSideNav.js.jsx
@@ -0,0 +1,74 @@
+import React, { PropTypes } from 'react';
+import Base from '../../../global/components/BaseComponent.js.jsx';
+import { Router, Route, Link, IndexLink } from 'react-router';
+import { connect } from 'react-redux';
+// import Study from "../StudyHandler";
+// import Site from "../../site/SiteHandler";
+
+// import actions
+import * as userActions from '../../user/userActions';
+
+class SideNav extends Base {
+  constructor(props, context) {
+    super(props);
+    this._bind(
+      '_handleLogout'
+    );
+  }
+
+  _handleLogout() {
+    const { dispatch } = this.props;
+    dispatch(userActions.sendLogout()).then((res) => {
+      if(res.success) {
+        //redirect
+        browserHistory.push('/');
+      } else {
+        alert("ERROR LOGGING OUT - " + res.message);
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div className="sidebar">
+        <div className="nav-header"> Admin Dashboard </div>
+        <ul className="side-nav">
+          <li>
+            <Link to="/admin/style-guide" activeClassName="active" >Styleguide</Link>
+          </li>
+        </ul>
+        <hr/>
+        <ul className="side-nav">
+          <li>
+            <Link to="/admin/users" activeClassName="active" >Yote Users</Link>
+          </li>
+
+        </ul>
+        <hr/>
+        <ul className="side-nav">
+          <li>
+            <a onClick={this._handleLogout}>Logout</a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
+}
+
+SideNav.propTypes = {
+  dispatch: PropTypes.func.isRequired
+}
+
+
+const mapStoreToProps = (state) => {
+  console.log("list state");
+  console.log(state);
+  return {
+  }
+}
+
+export default connect(mapStoreToProps
+, null, null, {
+  pure:false
+})(SideNav);
