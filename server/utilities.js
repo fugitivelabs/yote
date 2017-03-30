e53f0042dabe66a8ef24b764b599e84f1cacdd94
Erik Guffrey <erik@fugitivelabs.com>
2017-03-30 10:44:13 -0400

    removing a bunch of commented out dead code

--- server/utilities.js
+++ server/utilities.js
@@ -24,13 +24,19 @@ exports.sendEmail = function(targets, subject, content, callback) {
     , subject: subject
     , html: content
   }
+
+  // build recipients list
   for(var i = 0; i < targets.length; i++) {
     msg.to.push({
       "type": "to"
       , "email": targets[i]
     });
   }
+  
+  // // for testing: don't actually send any emails
+  // callback({success: true, message: "email(s) sent", result: {}});
 
+  // for deployment: actually send emails
   mandrill_client.messages.send({ "message": msg, "async": false }, function(result) {
     logger.debug(result);
     callback({success: true, message: "email(s) sent", result: result});
@@ -41,4 +47,48 @@ exports.sendEmail = function(targets, subject, content, callback) {
   });
 }
 
-// TODO: bring in sendEmailTemplate
+
+exports.sendEmailTemplate = function(targets, subject, template_name, template_content, html, text, fromInfo, callback) {
+  logger.debug("trying to send email");
+
+  // message fields
+  var msg = {
+    important: true
+    , track_opens: true
+    , track_clicks: true
+    , auto_text: true
+    , auto_html: true
+    , preserve_recipients: false
+    , view_content_link: true
+    , signing_domain: "soflete.com"
+    , from_email: "train@soflete.com"
+    , from_name: "SOFLETE"
+    , to: []
+    , subject: subject
+    , html: html
+    , text: text
+  }
+
+  // build recipients list
+  for(var i = 0; i < targets.length; i++) {
+    msg.to.push({
+      "type": "to"
+      , "email": targets[i]
+    });
+  }
+
+  // // for testing: don't actually send any emails
+  // callback({success: true, message: "email(s) sent", result: {}});
+
+  // for deployment: actually send emails
+  mandrill_client.messages.sendTemplate({"template_name": template_name, "template_content": template_content, "message": msg, "async": true }, function(result) {
+    logger.debug("mandrill send success");
+    logger.debug(result);
+    callback({success: true, message: "email(s) sent", result: result});
+  }, function(e) {
+    // Mandrill returns the error as an object with name and message keys
+    logger.error('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+    callback({success: false, message: "Failed to send email.", error: e});
+  });
+
+}
