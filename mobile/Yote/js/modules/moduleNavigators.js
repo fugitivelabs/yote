49fd3ec722a03e5c19c4c82740047c3aaadfec2d
Austin Cline <austincline09@users.noreply.github.com>
2017-10-12 11:35:58 -0400

    moduleNavigators comment

--- mobile/Yote/js/modules/moduleNavigators.js
+++ mobile/Yote/js/modules/moduleNavigators.js
@@ -1,3 +1,9 @@
-// import module navigator components
+/**
+ * Reads and exports the navigators as defined by each resource module
+ *
+ * To add a resource navigator to the tabs bar, import it from AppNavigator
+ *
+ * NOTE: this facilitates adding navigators via the CLI
+ */
 
 export { default as UserNavigator } from './user/UserNavigator'; 
