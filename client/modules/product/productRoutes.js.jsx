ccb18d9669e1b6db193a2184642ef6074b95e209
Erik Guffrey <erik@fugitivelabs.com>
2017-11-03 12:28:10 -0400

    cleanup product route file

--- client/modules/product/productRoutes.js.jsx
+++ client/modules/product/productRoutes.js.jsx
@@ -2,17 +2,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-// import utilities
-import Auth from '../../global/utils/auth';
-
-// import product components
-import CreateProduct from './components/CreateProduct.js.jsx';
+// import product layout
 import ProductLayout from './components/ProductLayout.js.jsx';
-import ProductList from './components/ProductList.js.jsx';
-import SingleProduct from './components/SingleProduct.js.jsx';
-import UpdateProduct from './components/UpdateProduct.js.jsx';
 
-// define product routes
+// initialize product routes
 const productRoutes =
   <Route
     component={ProductLayout}
