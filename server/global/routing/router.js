bcbfa426ed4c108129311369d561559affce73a3
Grant Fowler <grant@fugitivelabs.com>
2019-02-18 17:09:22 -0500

    packages and allow imports in node

--- server/global/routing/router.js
+++ server/global/routing/router.js
@@ -2,18 +2,18 @@
  * Configure the application routes
  */
 
-module.exports = function(router, app) {
+module.exports = (router, app) => {
 
   // require api routes list
   require('./api-router')(router);
 
   // catch all other api requests and send 404
-  router.all('/api/*', function(req, res) {
+  router.all('/api/*', (req, res) => {
     res.send(404);
   });
 
   // render layout
-  router.get('*', function(req, res) {
+  router.get('*', (req, res) => {
     res.render('layout', {
       currentUser: req.user
       , development: app.get('env') == 'development' ? true : false
