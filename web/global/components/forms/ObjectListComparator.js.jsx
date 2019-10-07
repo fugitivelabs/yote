7145efec2be184588a8b19310fc399e9692f0d19
Wes Jourdan <wes.jourdan@icloud.com>
2019-10-07 16:34:36 -0400

    Add updated ObjectListComparator.

--- web/global/components/forms/ObjectListComparator.js.jsx
+++ web/global/components/forms/ObjectListComparator.js.jsx
@@ -4,6 +4,7 @@ import Binder from '../Binder.js.jsx';
 
 // import third-party libraries
 import _ from 'lodash';
+import ReactTooltip from 'react-tooltip';
 
 // import form components
 import TextInput from './TextInput.js.jsx';
@@ -16,10 +17,12 @@ class ObjectListComparator extends Binder {
     }
     this._bind(
       '_addItem'
-      , '_handleFormChange'
+      , '_addAll'
       , '_moveDown'
       , '_moveUp'
       , '_removeItem'
+      , '_removeAll'
+      , '_handleFormChange'
     )
   }
 
@@ -28,6 +31,13 @@ class ObjectListComparator extends Binder {
     // console.log("REMOVE " + index);
     let selected = this.props.selected;
     selected.splice(index, 1);
+    let event = {target: {name: this.props.name, value: this.props.selected} };
+    this.props.change(event);
+  }
+
+  _removeAll() {
+    let selected = this.props.selected
+    selected.splice(0, selected.length)
     let event = {target: {name: this.props.name, value: this.props.selected} };
     this.props.change(event);
   }
@@ -63,6 +73,18 @@ class ObjectListComparator extends Binder {
       }
     }
     newSelected.push(unselectedItems[index][valueKey]);
+    let event = {target: {name: this.props.name, value: newSelected} };
+    this.props.change(event);
+  }
+
+  _addAll() {
+    const { items, selected, valueKey } = this.props;
+    let newSelected = selected 
+    for (let i = 0; i < items.length; i++) {
+      if(!newSelected.includes(items[i][valueKey])) {
+        newSelected.push(items[i][valueKey])
+      }
+    }
     let event = {target: {name: this.props.name, value: newSelected} };
     this.props.change(event);
   }
@@ -108,16 +130,23 @@ class ObjectListComparator extends Binder {
   render() {
     // console.log("object list comparator rendering");
     const {
-      displayKey
+      addLabel
+      , addRemoveAll
+      , displayKey
       , filterable
+      , tooltipText
       , items
       , label
       , name
+      , removeLabel
       , reorderable
       , selected
+      , selectedLabel
+      , availableLabel
       , valueKey
     } = this.props;
     const { queryText } = this.state;
+    const tooltipId = Math.floor(Math.random()*16777215).toString(16);
 
     let filteredItems = [];
     if(filterable) {
@@ -155,74 +184,117 @@ class ObjectListComparator extends Binder {
 
     return (
       <div className="input-group">
-        <label htmlFor="newItem"> { label } </label>
+        <label htmlFor="newItem">
+        { label }
+        { tooltipText ?
+            <span>
+              <sup><i className="fa fa-info-circle" style={{paddingLeft: '.5em'}} data-tip data-for={tooltipId} /></sup>
+              <ReactTooltip
+                aria-haspopup='true'
+                className="yt-tooltip"
+                id={tooltipId}
+                place="top"
+                type="dark"
+              >
+                <p>{tooltipText}</p>
+              </ReactTooltip>
+            </span>
+          :
+            null
+        }
+        </label>
         <div className="list-comparator-input">
           <div className="yt-row with-gutters">
             { filterable ?
-              <div className="yt-col _100">
-                <TextInput
-                  name="queryText"
-                  label="Filter"
-                  value={queryText}
-                  change={this._handleFormChange}
-                  placeholder="Filter"
-                  required={false}
-                />
-              </div>
-              : 
-              null 
+                <div className="yt-col _100">
+                  <TextInput
+                    name="queryText"
+                    label="Filter"
+                    value={queryText}
+                    change={this._handleFormChange}
+                    placeholder="Filter"
+                    required={false}
+                  />
+                </div>
+              :
+                <div className="yt-col _100"></div>
             }
           </div>
           <div className="yt-row with-gutters">
             <div className="yt-col _50">
-              <div className="-label">Selected:</div>
+              <div className="-label">{selectedLabel ? selectedLabel + ':': 'Selected:'}</div>
+              { addRemoveAll ?
+                <button
+                  type="button"
+                  className="yt-btn link info x-small"
+                  onClick={() => this._removeAll()}
+                >
+                  {removeLabel ? removeLabel : "Remove All"}
+                </button>
+                : null
+              }
+                
               <div className="-list-items">
                 { selectedItems.map((item, i) => {
-                  return(<div key={i}  className="-item  -left">
-                    <div className="text">
-                      {item[displayKey] + "    "}
+                  return(
+                    <div key={i}  className="-item  -left">
+                      <div className="text">
+                        {item[displayKey] + "    "}
+                      </div>
+                      { i > 0 && reorderable ?
+                        <button
+                          type="button"
+                          className="yt-btn link primary x-small"
+                          onClick={() => this._moveUp(i)}
+                        >
+                          <i className="fa fa-chevron-up"/>
+                        </button>
+                        : null
+                      }
+                      { i < (items.length -1) && selectedItems.length > 1  && reorderable ?
+                        <button
+                          type="button"
+                          className="yt-btn link primary x-small"
+                          onClick={() => this._moveDown(i)}
+                        >
+                          <i className="fa fa-chevron-down"/>
+                        </button>
+                        : null
+                      }
+                      <button
+                        type="button"
+                        className="yt-btn link danger x-small"
+                        onClick={() => this._removeItem(i)}
+                      >
+                        <i className="fas fa-times" />
+                      </button>
                     </div>
-                    { i > 0 && reorderable ?
-                      <button
-                        type="button"
-                        className="yt-btn link primary x-small"
-                        onClick={()=> this._moveUp(i)}
-                      >
-                        <i className="ion ion-chevron-up"/>
-                      </button>
-                      : null
-                    }
-                    { i < (items.length -1) && reorderable ?
-                      <button
-                        type="button"
-                        className="yt-btn link primary x-small"
-                        onClick={()=> this._moveDown(i)}
-                      >
-                        <i className="ion ion-chevron-down"/>
-                      </button>
-                      : null
-                    }
-                    <button
-                      type="button"
-                      className="yt-btn link danger x-small"
-                      onClick={()=> this._removeItem(i)}
-                    >
-                      <i className="ion ion-close-round" />
-                    </button>
-                  </div>)
+                  )
                 })}
               </div>
             </div>
             <div className="yt-col _50">
-              <div className="-label">Available:</div>
+              <div className="-label">{availableLabel ? availableLabel + ':' : 'Available:'}</div>
+              { addRemoveAll ?
+                <button
+                  type="button"
+                  className="yt-btn link info x-small"
+                  onClick={() => this._addAll()}
+                >
+                  {addLabel ? addLabel : 'Add All'}
+                </button>
+                : null
+              }
               <div className="-list-items ">
                 { unselectedItems.map((item, i) => {
-                  return(<div key={i}  className="-item -right" onClick={() => this._addItem(i)}>
-                    <i className="fa fa-arrow-circle-left"></i>
-                    <div className="-text">
-                      {"    " + item[displayKey]}
+                  return(
+                    <div key={i}  className="-item -right" onClick={() => this._addItem(i)}>
+                      <i className="fa fa-arrow-circle-left"></i>
+                      <div className="-text">
+                        {"    " + item[displayKey]}
+                      </div>
                     </div>
-                  </div>)
+                  )
                 })}
               </div>
             </div>
@@ -245,6 +317,7 @@ ObjectListComparator.propTypes = {
   , name: PropTypes.string.isRequired // name of field on parent to update
   , reorderable: PropTypes.bool
   , selected: PropTypes.arrayOf(PropTypes.string) // array of [valueKey's]
+  , tooltipText: PropTypes.string
   , valueKey: PropTypes.string.isRequired // key to return as selected
 }
 
