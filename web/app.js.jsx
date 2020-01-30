2310bc53550659a6bd119fc6040e0cff950e0762
Grant Fowler <grant@fugitivelabs.com>
2020-01-30 09:54:26 -0500

    more and depreciation warnings

--- web/app.js.jsx
+++ web/app.js.jsx
@@ -13,7 +13,7 @@ require('es5-shim/es5-sham');
 // import primary libraries
 import React from 'react';
 import { render } from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -21,7 +21,9 @@ import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
 
 // instantiate tap event plugin
-injectTapEventPlugin();
+// https://www.npmjs.com/package/react-tap-event-plugin
+// fixed in most browser, recommended to migrate away from this plugin
+// injectTapEventPlugin();
 
 
 // import scss files.
