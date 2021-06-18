29ddfec7c7c5b0ee4a7a0892bb3571968636be1c
Wes Jourdan <wes.jourdan@icloud.com>
2021-06-18 13:03:52 -0400

    more comments explaining custom hooks

--- web-experimental/src/global/utils/customHooks.js
+++ web-experimental/src/global/utils/customHooks.js
@@ -2,9 +2,17 @@ import React, { useReducer } from 'react'
 
 // This little guy handles editing a resource in component state before sending it to the server.
 // Includes the standard handleChange method so we don't have to define it everywhere.
+
+/**
+ * This little guy handles editing a resource in component state before sending it to the server.
+ * @param {object} initialState - the object to be updated
+ * @returns [ state, handleChange ]
+ */
 export const useForm = (initialState = {}) => {
-  // The reducer determines what we do when new values come in.
+  // The reducer determines what we do when new values come in
+  // In the case of a form we want to update the state object with the new values.
   const reducer = (state, newState) => ({ ...state, ...newState })
+  
   // useReducer returns a state variable and a function to update it.
   // It is almost the same as useState, but for objects instead of primitives.
   const [state, setState] = useReducer(reducer, initialState);
