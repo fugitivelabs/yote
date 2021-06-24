dd9f7f8342d1e58b9e3b1ccbd42cfaba7f51165c
Wes Jourdan <wes.jourdan@icloud.com>
2021-06-24 16:29:07 -0400

    add AsyncWrapper (don't love the name)

---
+++ web-experimental/src/global/components/helpers/AsyncWrapper.js.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+
+// deals with fetch info supplied by query hooks and displays loading and error states if applicable.
+// only renders children when the fetch is done.
+const AsyncWrapper = ({
+  children, // all children of this component
+  // the rest of these are provided from the fetch object passed in (the one returned by the service)
+  error: dataError,
+  isLoading,
+  isFetching,
+  refetch,
+}) => {
+  try {
+    // there was an error fetching data
+    if(dataError) return <div className={'error'}>Oops, there was an error accessing this data. {refetch && <button onClick={refetch}>Try again</button>}</div>
+    // still waiting for data
+    if(isLoading) return <div className={'loading'}>Loading...</div>
+    // fetch is done. render children to display the fetched data
+    return (
+      <div className={isFetching ? 'opacity-50' : ''}>
+        {children}
+      </div>
+    )
+  } catch(childError) {
+    // there was an error thrown by the children, but the app will not crash, it will display an error message instead.
+    return <div className={'error'}>Oops, there was an error. {refetch && <button onClick={refetch}>Try again</button>}</div>
+  }
+}
+
+export default AsyncWrapper;
