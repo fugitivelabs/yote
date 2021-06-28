4b10dded8fea46662a11b99532c984d0d7b2c029
Wes Jourdan <wes.jourdan@icloud.com>
2021-06-28 17:03:17 -0400

    add fallback and spinner for async wrapper

--- web-experimental/src/global/components/helpers/AsyncWrapper.js.jsx
+++ web-experimental/src/global/components/helpers/AsyncWrapper.js.jsx
@@ -7,6 +7,7 @@ const AsyncWrapper = ({
   children, // all children of this component
   // the rest of these are provided from the fetch object passed in (the one returned by the service)
   isError: dataError,
+  fallback = (<Spinner/>),
   isLoading,
   isFetching,
   refetch,
@@ -15,17 +16,20 @@ const AsyncWrapper = ({
     // there was an error fetching data
     if(dataError) return <div className={'error'}>Oops, there was an error accessing this data. {refetch && <button onClick={refetch}>Try again</button>}</div>
     // still waiting for data
-    if(isLoading) return <div className={'loading'}>Loading...</div>
+    if(isLoading) return fallback
     // fetch is done. render children to display the fetched data
-    return (
-      <div className={isFetching ? 'opacity-50' : ''}>
-        {children}
-      </div>
-    )
+    return children;
   } catch(childError) {
     // there was an error thrown by the children, but the app will not crash, it will display an error message instead.
+    // Could somehow log this error or save it as a userEvent kind of thing. Could make it easier to track bugs over time.
     return <div className={'error'}>Oops, there was an error. {refetch && <button onClick={refetch}>Try again</button>}</div>
   }
+}
+
+const Spinner = () => {
+  return (
+    <div className='animate-spin rounded-full border-8 border-gray-200 h-20 w-20 mx-auto' style={{borderTopColor: "lightblue"}}/>
+  )
 }
 
 export default AsyncWrapper;
