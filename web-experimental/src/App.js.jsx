32708dcc031ce4907ca2ac69bad32d2738a7d606
Wes Jourdan <wes.jourdan@icloud.com>
2021-06-29 17:03:19 -0400

    add redirect to products for testing

--- web-experimental/src/App.js.jsx
+++ web-experimental/src/App.js.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import './App.css';
 import ProductRouter from './resources/product/ProductRouter.js.jsx';
 import ProductRouter2 from './resources/product2/ProductRouter.js.jsx';
@@ -7,6 +8,8 @@ function App() {
   return (
     <>
       {/* import all resource routers here */}
+      {/* redirect for now since there is no view for the base route */}
+      <Redirect to="/products2"/>
       <ProductRouter />
       <ProductRouter2 />
       <UserRouter />
