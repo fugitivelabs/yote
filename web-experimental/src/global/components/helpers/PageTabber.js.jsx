8cb8a269df7160703632215864ace0ef3b843013
Wes Jourdan <wes.jourdan@icloud.com>
2021-06-29 10:00:34 -0400

    Add tailwind styling for PageTabber

--- web-experimental/src/global/components/helpers/PageTabber.js.jsx
+++ web-experimental/src/global/components/helpers/PageTabber.js.jsx
@@ -1,10 +1,19 @@
-
+// @ts-check
 import React from 'react'
 
 import PropTypes from 'prop-types';
 
-// import third-party libraries
-// import classNames from 'classnames';
+/* This example requires Tailwind CSS v2.0+ */
+// import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+
+// condense crazy tailwind styles for pagination component. Adapted from: https://tailwindui.com/components/application-ui/navigation/pagination#component-69eb9381f977800aa890ce8f7d9e2d20
+const paginationBtnCommonClasses = "relative inline-flex items-center px-4 py-2 text-sm font-medium border border-gray-300 bg-white"
+
+const placeholderBtnCommonClasses = `${paginationBtnCommonClasses} text-gray-700`
+
+const mobileBtnCommonClasses = `${placeholderBtnCommonClasses} rounded-md hover:bg-gray-50 cursor-pointer`
+
+const setPageBtnCommonClasses = `${paginationBtnCommonClasses} text-gray-500 hover:bg-gray-50 cursor-pointer`
 
 const PageTabber = ({
   pagination
@@ -45,47 +54,94 @@ const PageTabber = ({
   }
 
   return (
-    <ul className="pagination">
-      <li >
+    <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+      <div className="flex-1 flex justify-between sm:hidden">
         <a
-          className={`prev-page ${ currentPage == 1 && 'pointer-events-none'}`}
           onClick={currentPage > 1 ? () => setPage(currentPage - 1) : null }
+          className={mobileBtnCommonClasses}
         >
-          <i className="fa fa-angle-double-left" /> Previous
+          Previous
         </a>
-      </li>
-      { currentPage > 4 ?
-        <li>...</li>
-        :
-        null
-      }
-      { before.map((page, i) =>
-        <li key={i} >
-          <a className="page-num" onClick={()=> setPage(page)}>{page}</a>
-        </li>
-      )}
-      <li >
-        <span className="current-page">{currentPage}</span>
-      </li>
-      { after.map((page, i) =>
-        <li key={i} >
-          <a className="page-num" onClick={()=> setPage(page)}>{page}</a>
-        </li>
-      )}
-      { currentPage < totalPages - 3 ?
-        <li>...</li>
-        :
-        null
-      }
-      <li >
         <a
-          className={`next-page ${ currentPage == totalPages && 'pointer-events-none'}`}
           onClick={currentPage < totalPages ? () => setPage(currentPage + 1) : null }
+          className={`${mobileBtnCommonClasses} ml-3`}
         >
-          Next <i className="fa fa-angle-double-right" />
+          Next
         </a>
-      </li>
-    </ul>
+      </div>
+      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+        <div>
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">{ (pagination.page * pagination.per) - (pagination.per - 1)}</span> to <span className="font-medium">{ pagination.page * pagination.per}</span> of{' '}
+            <span className="font-medium">about {pagination.per * totalPages}</span> results
+          </p>
+        </div>
+        <div>
+          <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+            <a
+              onClick={currentPage > 1 ? () => setPage(currentPage - 1) : null}
+              className={`${setPageBtnCommonClasses} rounded-l-md` }
+            >
+              <span className="sr-only">Previous</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+              </svg>
+              {/* <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" /> */}
+            </a>
+            {currentPage > 4 ?
+              <span className={placeholderBtnCommonClasses}>
+                ...
+              </span>
+              :
+              null
+            }
+            {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+            
+            {before.map((page, i) => (
+              <a
+                key={`page-before-${i}`}
+                onClick={() => setPage(page)}
+                className={setPageBtnCommonClasses}
+              >
+              {page}
+              </a>
+            ))}
+            <a
+              aria-current="page"
+              className={`${paginationBtnCommonClasses} z-10 bg-indigo-50 border-indigo-500 text-indigo-600 pointer-events-none`}
+            >
+              {currentPage}
+            </a>
+            {after.map((page , i)=> (
+              <a
+                key={`page-after-${i}`}
+                onClick={()=> setPage(page)}
+                className={setPageBtnCommonClasses}
+              >
+              {page}
+              </a>
+            ))}
+            { currentPage < totalPages - 3 ?
+              <span className={placeholderBtnCommonClasses}>
+                ...
+              </span>
+              :
+              null
+            }
+            <a
+              onClick={currentPage < totalPages ? () => setPage(currentPage + 1) : null }
+              className={`${setPageBtnCommonClasses} rounded-r-md` }
+            >
+              <span className="sr-only">Next</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+              </svg>
+              {/* <ChevronRightIcon className="h-5 w-5" aria-hidden="true" /> */}
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
   )
 }
 
