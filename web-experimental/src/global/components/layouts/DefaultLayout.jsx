f8b80fefc26a1c4a9998756e631555c508cf133e
Wes Jourdan <wes.jourdan@icloud.com>
2021-09-07 16:33:38 -0400

    default layout skeleton

--- web-experimental/src/global/components/layouts/DefaultLayout.jsx
+++ web-experimental/src/global/components/layouts/DefaultLayout.jsx
@@ -5,6 +5,7 @@
 
 // import primary libraries
 import React, { useEffect } from 'react';
+import Spinner from '../helpers/Spinner.jsx';
 // TODO: Maybe find an alternative to react-helmet since it's out of date and causes react to throw warnings
 // import { Helmet }  from 'react-helmet'
 
@@ -40,5 +41,27 @@ const DefaultLayout = ({ ...props }) => {
     </div>
   )
 }
+
+const Skeleton = () => {
+  return (
+    <div>
+      <DefaultTopNav />
+      <header className="animate-pulse bg-white shadow-sm" >
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-lg h-6 leading-6 font-semibold text-gray-900">
+            <p className="w-3/12 h-5 bg-gray-500"></p>
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <Spinner/>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+DefaultLayout.Skeleton = Skeleton;
 
 export default DefaultLayout;
