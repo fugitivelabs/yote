2be533cc40e05ca736dce380ade8b38536dd8ba1
Chance Griffin <cbg@hey.com>
2021-09-28 09:35:54 -0400

    basic utilities on landing.jsx

--- web/src/global/components/landing/Landing.jsx
+++ web/src/global/components/landing/Landing.jsx
@@ -4,8 +4,10 @@ import DefaultLayout from '../layouts/DefaultLayout';
 const Landing = () => {
   return (
     <DefaultLayout title="Yote React Web">
-      <h1 className="text-center"> Welcome to Yote!</h1>
-      <p className="text-center"> Check out the docs on <a href="https://fugitivelabs.github.io/yote/"> GitHub </a></p>
+      <section class="p-2">
+        <h1 className="md:text-center text-3xl"> Welcome to Yote!</h1>
+        <p className="md:text-center"> Check out the docs on <a href="https://fugitivelabs.github.io/yote/"> GitHub </a></p>
+      </section>
     </DefaultLayout>
   )
 }
