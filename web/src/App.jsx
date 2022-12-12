ac2ffa58a00752e50db39ab448a6a0d33134796c
Wes Jourdan <Wes@PRFC-MAC-L011.local>
2022-12-12 14:00:33 -0700

    add lazy loading on resource routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './App.css';
 
 import routes from './config/routes';
+import DefaultLayout from './global/components/layouts/DefaultLayout';
 
 function App() {
+  useEffect(() => {
+    // we are using lazy loading to load our resource routers to reduce the initial bundle size
+    // we can preload the resource routers here so they are ready to go once the user needs them
+    import('./resources/product/ProductRouter');
+    import('./resources/user/UserRouter');
+    // add any other top level resource routers here that you want to preload
+  }, []) // run once on mount
+
+  // because we are using lazy loading at the route level, we need to wrap our routes in a Suspense component which will show a fallback while the route is loading
+  // if we want a more specific fallback somewhere down the tree, we can add a Suspense component there as well (most likely in an individual resource router)
   return (
-    <>
+    <Suspense fallback={<DefaultLayout.Skeleton />}>
       {routes}
-    </>
+    </Suspense>
   );
 }
 
